a0f0d000b3ac1ec82b889d4d987808ec5f1662c0
unknown <hhamdan.edu@gmail.com>
2022-03-28 08:55:11 +0200

    update package.json

--- server/server.js
+++ server/server.js
@@ -3,10 +3,10 @@ const { FRAME_RATE } = require('./constants');
 const { makeid } = require('./utils');
 
 // Setup server
-const express = require('express');
-const app = express();
+//const express = require('express');
+//const app = express();
 const http = require('http');
-const server = http.createServer(app);
+const server = http.createServer(http);
 const PORT = 3000 || process.env.PORT; // process.env.PORT is related to deploying on Heroku
 
 // Set static directory
